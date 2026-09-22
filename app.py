e20fea69046b9cbfbcf8ce0c1e2f5ab9af0de95d
Mohammed Mohidee M Z <mzmmohideen@gmail.com>
2026-09-22 10:24:03 +0530

    added pandas code

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sales_data = pd.DataFrame({
+    "Month": ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    "Sales": [40000, 45000, 42000, 52000, 60000, 58000]
+})
+
+sales_data
+
